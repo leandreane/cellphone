54eb6c828ad963deda91fba187650c6800c69d4b
leandreane <monk-86@inbox.ru>
2023-06-09 11:18:40 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int score = 200; // Первоночальный счёт
-        int deposit2 = 1350; // Сумма пополнения
+        int deposit = 1350; // Сумма пополнения
 
-        if (deposit2 > 1000) {
-
-            System.out.println("Итоговый счёт при пополнении на 1350 рублей: ");
-            System.out.println((deposit2 / 100) + score + deposit2 + " рублей ");
-            System.out.println();
-            System.out.println("Бонусных рублей при пополнении на 1350 рублей: ");
-            System.out.println(deposit2 / 100 + " рублей ");
-        }
+        System.out.println("Итоговый счёт при пополнении на 1350 рублей: ");
+        System.out.println((deposit / 100) + score + deposit + " рублей ");
+        System.out.println();
+        System.out.println("Бонусных рублей при пополнении на 1350 рублей: ");
+        System.out.println(deposit / 100 + " рублей ");
 
 
     }
